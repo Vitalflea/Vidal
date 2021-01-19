b02f8aa33e829e79efdf0f29e8cb74f8f76c689f
Vitalflea <sccarls1@yahoo.com>
2021-01-18 17:50:49 -0800

    upload

---
+++ Vidal/deps/curl/dkstd_string.hpp
@@ -0,0 +1,221 @@
+// author:      Khiêm Đoàn Hoà
+// created:     2016-03-19
+// modified:    2017-02-04
+
+#ifndef _DKSTD_STRING_
+#define _DKSTD_STRING_
+
+#include "codecvt"
+#include "algorithm"
+#include "memory"
+#include "stdexcept"
+#include "locale"
+
+namespace dkstd
+{
+    std::wstring s2ws(std::string str);
+    std::string ws2s(std::wstring wstr);
+
+    template<typename ...Args>
+    std::string format_string(const std::string& format, Args ...args);
+    template<typename ...Args>
+    std::wstring format_string(const std::wstring& format, Args ...args);
+
+    namespace string
+    {
+        template<typename charT>
+        std::basic_string<charT> to_lower(std::basic_string<charT> sInput);
+        template<typename charT>
+        std::basic_string<charT> to_lower(charT* pInput);
+
+        template<typename charT>
+        std::basic_string<charT> to_upper(std::basic_string<charT> sInput);
+        template<typename charT>
+        std::basic_string<charT> to_upper(charT* pInput);
+
+        template<typename charT>
+        int icompare(std::basic_string<charT> sL, std::basic_string<charT> sR);
+        template<typename charT>
+        int icompare(charT* pL, charT* pR);
+
+        template<typename charT>
+        std::size_t ifind(std::basic_string<charT> sStr, std::basic_string<charT> sSubStr, std::size_t pos = 0);
+        template<typename charT>
+        std::size_t ifind(charT* pStr, charT* pSubStr, std::size_t pos = 0);
+
+        template<typename charT>
+        std::size_t irfind(std::basic_string<charT> sStr, std::basic_string<charT> sSubStr, std::size_t pos = std::basic_string<charT>::npos);
+        template<typename charT>
+        std::size_t irfind(charT* pStr, charT* pSubStr, std::size_t pos = std::basic_string<charT>::npos);
+    }
+}
+
+// convert std::string to std::wstring
+// KhiemDH - 2016-08-14
+inline std::wstring dkstd::s2ws(std::string str)
+{
+    using convert_type = std::codecvt_utf8<wchar_t>;
+    std::wstring_convert<convert_type, wchar_t> converter;
+    std::wstring sOutput;
+    try {
+        sOutput = converter.from_bytes(str);
+    }
+    catch (const std::range_error e) {}
+    return sOutput;
+}
+
+// convert std::wstring to std::string
+// KhiemDH - 2016-08-14
+inline std::string dkstd::ws2s(std::wstring wstr)
+{
+    using convert_type = std::codecvt_utf8<wchar_t>;
+    std::wstring_convert<convert_type, wchar_t> converter;
+    std::string sOutput;
+    try {
+        sOutput = converter.to_bytes(wstr);
+    }
+    catch (const std::range_error e) {}
+    return sOutput;
+}
+
+// format string
+// KhiemDH - 2017-02-04
+template<typename ...Args>
+std::string dkstd::format_string(const std::string & format, Args ...args)
+{
+    std::string sReturn;
+#ifdef _WIN32
+    int size = _scprintf(format.c_str(), args...) + 1;                      // Extra space for '\0'
+#else
+    int size = std::snprintf(nullptr, 0, format.c_str(), args...) + 1;      // Extra space for '\0'
+#endif
+    std::unique_ptr<char[]> buf(new char[size]);
+    size = std::snprintf(buf.get(), size, format.c_str(), args...);
+    if (size > 0)
+    {
+        sReturn = std::string(buf.get(), buf.get() + size);
+    }
+    return sReturn;
+}
+
+// format string
+// KhiemDH - 2017-02-04
+template<typename ...Args>
+std::wstring dkstd::format_string(const std::wstring & format, Args ...args)
+{
+    std::wstring sReturn;
+#ifdef _WIN32
+    int size = _scwprintf(format.c_str(), args...) + 1;                     // Extra space for '\0'
+#else
+    int size = std::swprintf(nullptr, 0, format.c_str(), args...) + 1;      // Extra space for '\0'
+#endif
+    std::unique_ptr<wchar_t[]> buf(new wchar_t[size]);
+    size = std::swprintf(buf.get(), size, format.c_str(), args...);
+    if (size > 0)
+    {
+        sReturn = std::wstring(buf.get(), buf.get() + size);
+    }
+    return sReturn;
+}
+
+// convert to lower string
+// KhiemDH - 2016-07-13
+template<typename charT>
+std::basic_string<charT> dkstd::string::to_lower(std::basic_string<charT> sInput)
+{
+    std::basic_string<charT> sOutput;
+    sOutput.resize(sInput.size());
+    std::transform(sInput.begin(), sInput.end(), sOutput.begin(), ::tolower);
+    return sOutput;
+}
+
+// convert to lower string
+// KhiemDH - 2016-08-15
+template<typename charT>
+std::basic_string<charT> dkstd::string::to_lower(charT * pInput)
+{
+    std::basic_string<charT> sInput(pInput);
+    return dkstd::string::to_lower(sInput);
+}
+
+// convert to upper string
+// KhiemDH - 2016-07-13
+template<typename charT>
+std::basic_string<charT> dkstd::string::to_upper(std::basic_string<charT> sInput)
+{
+    std::basic_string<charT> sOutput;
+    sOutput.resize(sInput.size());
+    std::transform(sInput.begin(), sInput.end(), sOutput.begin(), ::toupper);
+    return sOutput;
+}
+
+// convert to upper string
+// KhiemDH - 2016-08-15
+template<typename charT>
+std::basic_string<charT> dkstd::string::to_upper(charT * pInput)
+{
+    std::basic_string<charT> sInput(pInput);
+    return dkstd::string::to_upper(sInput);
+}
+
+// case insensitive compare string
+// KhiemDH - 2016-09-10
+template<typename charT>
+int dkstd::string::icompare(std::basic_string<charT> sL, std::basic_string<charT> sR)
+{
+    sL = dkstd::string::to_lower(sL);
+    sR = dkstd::string::to_lower(sR);
+    return sL.compare(sR);
+}
+
+// case insensitive compare string
+// KhiemDH - 2016-09-10
+template<typename charT>
+int dkstd::string::icompare(charT * pL, charT * pR)
+{
+    std::basic_string<charT> sL(pL);
+    std::basic_string<charT> sR(pR);
+    return dkstd::string::icompare(sL, sR);
+}
+
+// case insensitive finds the first substring
+// KhiemDH - 2016-10-29
+template<typename charT>
+std::size_t dkstd::string::ifind(std::basic_string<charT> sStr, std::basic_string<charT> sSubStr, std::size_t pos)
+{
+    sStr = dkstd::string::to_lower(sStr);
+    sSubStr = dkstd::string::to_lower(sSubStr);
+    return sStr.find(sSubStr, pos);
+}
+
+// case insensitive finds the first substring
+// KhiemDH - 2016-10-29
+template<typename charT>
+std::size_t dkstd::string::ifind(charT * pStr, charT * pSubStr, std::size_t pos)
+{
+    std::basic_string<charT> sStr(pStr);
+    std::basic_string<charT> sSubStr(pSubStr);
+    return dkstd::string::ifind(sStr, sSubStr, pos);
+}
+
+// case insensitive finds the last substring
+// KhiemDH - 2016-10-29
+template<typename charT>
+std::size_t dkstd::string::irfind(std::basic_string<charT> sStr, std::basic_string<charT> sSubStr, std::size_t pos)
+{
+    sStr = dkstd::string::to_lower(sStr);
+    sSubStr = dkstd::string::to_lower(sSubStr);
+    return sStr.rfind(sSubStr, pos);
+}
+
+// case insensitive finds the last substring
+// KhiemDH - 2016-10-29
+template<typename charT>
+std::size_t irfind(charT* pStr, charT* pSubStr, std::size_t pos = 0)
+{
+    std::basic_string<charT> sStr(pStr);
+    std::basic_string<charT> sSubStr(pSubStr);
+    return dkstd::string::irfind(sStr, sSubStr, pos);
+}
+
+#endif // !_DKSTD_STRING_
---
+++ Vidal/deps/curl/dkstd_textfile.hpp
@@ -0,0 +1,201 @@
+// author:      Khiêm Đoàn Hoà
+// created:     2016-07-06
+// modified:    2016-09-24
+
+#ifndef _DKSTD_TEXTFILE_
+#define _DKSTD_TEXTFILE_
+
+#include "fstream"
+#include "vector"
+#include "dkstd_string.hpp"
+
+namespace dkstd
+{
+
+    // textfile is used to read and write file by utf-8
+    // KhiemDH - 2016-07-06
+    class textfile
+    {
+    public:
+        textfile() {};
+        textfile(std::string sFilePath);
+        textfile(std::wstring sFilePath);
+        textfile(std::string sFilePath, std::ios_base::openmode mode);
+        textfile(std::wstring sFilePath, std::ios_base::openmode mode);
+        ~textfile();
+
+        void	open(std::string sFilePath);
+        void	open(std::wstring sFilePath);
+        void	open(std::string sFilePath, std::ios_base::openmode mode);
+        void	open(std::wstring sFilePath, std::ios_base::openmode mode);
+        void	close();
+        
+        template<typename ...Args>
+        void write_line(std::string sFormat, Args ...args);
+        template<typename ...Args>
+        void write_line(std::wstring sFormat, Args ...args);
+
+        std::vector<std::wstring>	get_all_lines();
+
+        void clear();
+
+    private:
+        std::fstream		m_file;
+        std::ios::openmode	m_mode = std::fstream::in | std::fstream::out | std::fstream::app;
+#ifdef _WIN32
+        std::wstring		m_sFilePath;
+#else
+        std::string			m_sFilePath;
+#endif
+    };
+}
+
+// constructor with file path
+// KhiemDH - 2016-07-06
+inline dkstd::textfile::textfile(std::string sFilePath)
+{
+    this->open(sFilePath);
+}
+
+// constructor with file path
+// KhiemDH - 2016-07-06
+inline dkstd::textfile::textfile(std::wstring sFilePath)
+{
+    this->open(sFilePath);
+}
+
+// constructor with file path and open mode
+// KhiemDH - 2016-09-24
+inline dkstd::textfile::textfile(std::string sFilePath, std::ios_base::openmode mode)
+{
+    this->open(sFilePath, mode);
+}
+
+// constructor with file path and open mode
+// KhiemDH - 2016-09-24
+inline dkstd::textfile::textfile(std::wstring sFilePath, std::ios_base::openmode mode)
+{
+    this->open(sFilePath, mode);
+}
+
+// destructor
+// KhiemDH - 2016-07-06
+inline dkstd::textfile::~textfile()
+{
+    this->close();
+}
+
+// open file
+// KhiemDH - 2016-09-24
+inline void dkstd::textfile::open(std::string sFilePath)
+{
+    this->open(sFilePath, std::fstream::in | std::fstream::out | std::fstream::app);
+}
+
+// open file
+// KhiemDH - 2016-09-24
+inline void dkstd::textfile::open(std::wstring sFilePath)
+{
+    this->open(sFilePath, std::fstream::in | std::fstream::out | std::fstream::app);
+}
+
+// open file with custom mode
+// KhiemDH - 2016-09-24
+inline void dkstd::textfile::open(std::string sFilePath, std::ios_base::openmode mode)
+{
+#ifdef _WIN32
+    this->open(dkstd::s2ws(sFilePath), mode);
+#else
+    this->close();
+    m_file.open(sFilePath, mode);
+    m_sFilePath = sFilePath;
+    m_mode = mode;
+#endif
+}
+
+// open file with custom mode
+// KhiemDH - 2016-09-24
+inline void dkstd::textfile::open(std::wstring sFilePath, std::ios_base::openmode mode)
+{
+#ifdef _WIN32
+    this->close();
+    m_file.open(sFilePath, mode);
+    m_sFilePath = sFilePath;
+    m_mode = mode;
+#else
+    this->open(dkstd::ws2s(sFilePath), mode);
+#endif
+}
+
+// close file
+// KhiemDH - 2016-08-15
+inline void dkstd::textfile::close()
+{
+    if (m_file.is_open()) {
+        m_file.close();
+    }
+}
+
+// write a line to end file
+// input: ANSI string
+// KhiemDH - 2016-08-15
+template<typename ...Args>
+inline void dkstd::textfile::write_line(std::string sFormat, Args ...args)
+{
+    std::string sContent = dkstd::format_string(sFormat, args...);
+    if (m_file.good()) {
+        m_file.seekp(0, std::fstream::end);
+        if (m_file.tellg() != std::streampos::fpos(0)) {
+            sContent = "\n" + sContent;
+        }
+        m_file << sContent;
+        m_file.clear();		// clear the error flag
+    }
+}
+
+// write a line to end file
+// input: Unicode string
+// KhiemDH - 2016-07-06
+template<typename ...Args>
+inline void dkstd::textfile::write_line(std::wstring sFormat, Args ...args)
+{
+    std::wstring sContent = dkstd::format_string(sFormat, args...);
+    this->write_line(dkstd::ws2s(sContent));
+}
+
+// read file to std::vector
+// KhiemDH - 2016-08-15
+inline std::vector<std::wstring> dkstd::textfile::get_all_lines()
+{
+    std::string		sLine;
+    std::vector<std::wstring>	vectorContents;
+
+    if (m_file.good()) {
+        m_file.seekg(0, std::fstream::end);
+        if (m_file.tellg() > 0)
+        {
+            m_file.seekg(0, std::fstream::beg);
+            while (!m_file.eof()) {
+                std::getline(m_file, sLine);
+                if (sLine.length() > 0 && sLine.back() == '\r') {
+                    sLine.pop_back();
+                }
+                vectorContents.push_back(dkstd::s2ws(sLine));
+            }
+        }
+        m_file.clear();
+    }
+
+    return vectorContents;
+}
+
+// clear text in file
+inline void dkstd::textfile::clear()
+{
+    this->close();
+    m_file.open(m_sFilePath, std::fstream::out | std::fstream::trunc);
+    this->close();
+    this->open(m_sFilePath, m_mode);
+}
+
+#endif // !_DKSTD_TEXTFILE_
